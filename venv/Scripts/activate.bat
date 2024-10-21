8b6b76c09443a72982db6c197455eafc0c05ba35
Cyusaalain <umuhirecyusaa@gmail.com>
2024-10-21 09:14:52 +0200

    l

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC\Documents\GitHub\flask-quiz-app\venv
+set VIRTUAL_ENV=C:\Users\WECPC012\Documents\GitHub\flask-quiz-app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
